31823623e3977a1815f4a12a47df901b0b5fa9d2
tanujauppala <tanujauppala9@gmail.com>
2025-07-30 17:22:26 +0530

    iam-roles-assigned

--- main.tf
+++ main.tf
@@ -2,10 +2,9 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.0.0"
+      version = ">= 6.46.0"
     }
   }
-
   required_version = ">= 1.3.0"
 }
 provider "google" {
@@ -15,20 +14,65 @@ provider "google" {
 }
 locals {
   data = yamldecode(file("projects.yaml"))
-}
-resource "google_project" "projects" {
-  for_each = { for project in local.data.projects : project.projectId => project }
-
-  name       = each.value.name
-  project_id = each.value.projectId
-  labels     = each.value.labels
-  deletion_policy = var.delete_policy # Controls deletion behavior. Set variable 'delete_policy' to delete to allow deletion, or by default it will be prevent.
 
 }
-resource "google_project_iam_member" "projects_viewer" {
-  for_each = { for project in local.data.projects : project.projectId => project }
 
-  project = each.value.projectId
-  role    = "roles/viewer"
-  member  = "user:tanujagcp@gmail.com"
+
+# resource "google_project" "projects" {
+#   for_each = { for project in local.data.projects : project.projectId => project }
+
+#   name       = each.value.name
+#   project_id = each.value.projectId
+#   labels     = each.value.labels
+#   deletion_policy = var.delete_policy # Controls deletion behavior. Set variable 'delete_policy' to delete to allow deletion, or by default it will be prevent.
+
+# # }
+# resource "google_project_iam_member" "projects_viewer" {
+#   for_each = { for project in local.data.projects : project.projectId => project }
+
+#   project = each.value.projectId
+#   role    = "roles/resourcemanager.projectIamAdmin"
+#   member  = "user:tanujagcp@gmail.com"
+# }
+# # resource "google_project_iam_binding" "team_roles" {
+#   for_each = {
+#     for team in local.team_data.teams : team.name => team }
+
+#   project = each.value.project
+#   role    = each.value.role
+
+#   members = [
+#     for member in each.value.members : "user:${member.user}"
+#   ]
+# }
+
+
+locals {
+
+  team_data = yamldecode(file("roles.yaml"))
+
+  team_roles = flatten([
+    for team in local.team_data.teams : [
+      for member in team.members : [
+        for role in member.roles : {
+          project = team.project
+          user    = member.user
+          role    = role
+        }
+      ]
+    ]
+  ])
 }
+
+resource "google_project_iam_member" "team_roles" {
+  for_each = {
+    for role_assign in local.team_roles :
+    "${role_assign.project}-${role_assign.user}-${role_assign.role}" => role_assign
+  }
+
+  project = each.value.project
+  role    = each.value.role
+  member  = "user:${each.value.user}"
+}
+
+
